870bc808743a158d9a6e8ede2df3de6e5d6b7733
ksh040 <ksh0724abcd@gmail.com>
2026-08-28 12:22:52 +0900

    08/28

--- ch08/src/App.tsx
+++ ch08/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
-import LoginForm from './components/LoginForm'
+import Dynamic from './components/Dynamic'
+import LoginForm3 from './components/LoginForm3'
 
 
 function App() {
   return (
     <>
-      <LoginForm/>
+      <Dynamic/>
     </>
   )
 }
---
+++ ch08/src/hooks/useinput.ts
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function useInput(initialValue = ''){
+    const [value,setValue] = useState(initialValue);
+    const onChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+        setValue(e.target.value);
+    }
+    return {value,onChange}
+}
